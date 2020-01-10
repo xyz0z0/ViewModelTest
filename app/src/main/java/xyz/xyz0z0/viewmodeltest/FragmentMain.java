607f9928b53c967407334089b92396b8a7dde92d
Xyz0z0 <xyz0z0.xyz@gmail.com>
2020-01-10 22:10:52 +0800

    Add fragment

---
+++ app/src/main/java/xyz/xyz0z0/viewmodeltest/FragmentMain.java
@@ -0,0 +1,75 @@
+package xyz.xyz0z0.viewmodeltest;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+/**
+ * created by chengxugang on 2020/1/10
+ */
+public class FragmentMain extends Fragment {
+
+    private SharedViewModel model;
+    private Button btnAdd;
+    private FrameLayout flFirst;
+    private FrameLayout flSecond;
+    private FragmentFirst fragmentFirst;
+    private FragmentSecond fragmentSecond;
+    private FragmentThird fragmentThird;
+
+    @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        model = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
+        model.getSelected().observe(this, new Observer<Item>() {
+            @Override public void onChanged(Item item) {
+                Log.d("cxg", "First " + Thread.currentThread().getName());
+                Toast.makeText(getActivity(), "First", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void findView(View view) {
+        btnAdd = view.findViewById(R.id.btn_add);
+        flFirst = view.findViewById(R.id.fl_first);
+        flSecond = view.findViewById(R.id.fl_second);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        findView(view);
+        initView();
+        // tvText1 = view.findViewById(R.id.tv_text1);
+        return view;
+    }
+
+    private void initView() {
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override public void run() {
+                        getFragmentManager().beginTransaction().add(R.id.fl_container, fragmentThird)
+                            .addToBackStack("11").commit();
+                    }
+                }).start();
+            }
+        });
+        fragmentFirst = new FragmentFirst();
+        fragmentSecond = new FragmentSecond();
+        fragmentThird = new FragmentThird();
+        getFragmentManager().beginTransaction().add(R.id.fl_first, fragmentFirst).commit();
+        getFragmentManager().beginTransaction().add(R.id.fl_second, fragmentSecond).commit();
+
+    }
+
+}
--- app/src/main/java/xyz/xyz0z0/viewmodeltest/MainActivity.java
+++ app/src/main/java/xyz/xyz0z0/viewmodeltest/MainActivity.java
@@ -11,23 +11,19 @@ import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnAdd;
-    private FrameLayout flFirst;
-    private FrameLayout flSecond;
-    private FragmentFirst fragmentFirst;
-    private FragmentSecond fragmentSecond;
 
+
+    private FragmentMain fragmentMain;
     private SharedViewModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findView();
-        fragmentFirst = new FragmentFirst();
-        fragmentSecond = new FragmentSecond();
-        getSupportFragmentManager().beginTransaction().add(R.id.fl_first, fragmentFirst).commit();
-        getSupportFragmentManager().beginTransaction().add(R.id.fl_second, fragmentSecond).commit();
+
+
+        fragmentMain = new FragmentMain();
+        getSupportFragmentManager().beginTransaction().add(R.id.fl_container, fragmentMain).commit();
         model = ViewModelProviders.of(this).get(SharedViewModel.class);
         model.getName().observe(this, new Observer<String>() {
             @Override public void onChanged(String s) {
@@ -35,20 +31,8 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("cxg", "MainActivity " + Thread.currentThread().getName());
             }
         });
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                new Thread(new Runnable() {
-                    @Override public void run() {
-                        model.getSelected().postValue(new Item("杰克"));
-                    }
-                }).start();
-            }
-        });
+
     }
 
-    private void findView() {
-        btnAdd = findViewById(R.id.btn_add);
-        flFirst = findViewById(R.id.fl_first);
-        flSecond = findViewById(R.id.fl_second);
-    }
+
 }
